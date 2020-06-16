9ba92516eb525f3119fad23e689a879f30bae36b
Aaditya Kumar Rai <raiaaditya999@gmail.com>
2020-06-16 23:42:34 +0530

    adding a middleware

--- controllers/user.js
+++ controllers/user.js
@@ -52,6 +52,40 @@ exports.userPurchaseList = (req, res) =>{
     });
 };
 
+exports.pushOrderInPurchaseList = (req, res, next ) => {
+    let purchases = []
+    req.body.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        });
+        
+    });
+
+    //store this in DB
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: {purchases: purchases}},
+        {new: true},
+        (err, purchases) => {
+            if(err){
+                return res.status(400).json({
+                    error: "Unable to save PurcahseList"
+                })
+
+            }
+            next();
+
+        }
+    )
+
+}
+
 
 /*exports.getAllUsers = (req, res) =>{
     User.find().exec((err, manyusers) => {
