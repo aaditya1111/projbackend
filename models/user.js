dcf6f1efac921201c0a94b873f232fb2a18615cf
Aaditya Kumar Rai <raiaaditya999@gmail.com>
2020-06-22 20:37:50 +0530

    commit

--- models/user.js
+++ models/user.js
@@ -1,75 +1,74 @@
-var mongoose = require('mongoose');
-const crypto = require('crypto');
-const uuidv1 = require('uuid/v1');
+var mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
 
-
-  var userSchema = new mongoose.Schema({
-     name: {
-       type: String,
-       required: true,
-       maxlength: 32,
-       trim: true
-     },
-     lastname: {
-       type: String,
-       required: true,
-       maxlength: 32,
-       trim: true
-     },
-     email: {
-       type: String,
-       trim: true,
-       required: true,
-       unique: true
-     },
-     userinfo: {
-       type: String,
-       trim: true
-     },
-     encry_password: {
-       type: String,
-       required: true
-     },
-     salt: String,
-     role: {
-       type: Number,
-       default: 0
-     },
-     purchases: {
-       type: Array,
-       default:[]
-     }
-  }, { timestamps: true }
+var userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true
+    },
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true
+    },
+    userinfo: {
+      type: String,
+      trim: true
+    },
+    encry_password: {
+      type: String,
+      required: true
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0
+    },
+    purchases: {
+      type: Array,
+      default: []
+    }
+  },
+  { timestamps: true }
 );
 
-userSchema.virtual("password")
-  .set(function(password){
-    this._password = password
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
     this.salt = uuidv1();
     this.encry_password = this.securePassword(password);
   })
-  .get(function(){
-    return this._password
-  })
-
+  .get(function() {
+    return this._password;
+  });
 
 userSchema.methods = {
+  authenticate: function(plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
 
-   authenticate: function(plainpassword){
-     return this.securePassword(plainpassword) === this.encry_password
-   },
-
-    securePassword : function (plainpassword){
-      if(!plainpassword) return "";
-      try {
-        return crypto.createHmac('sha256', this.salt)
-                   .update('plainpassword')
-                   .digest('hex');
-      } catch (err) {
-        return "";
-      }
+  securePassword: function(plainpassword) {
+    if (!plainpassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
     }
-}
+  }
+};
 
-
-module.exports = mongoose.model("User", userSchema )
+module.exports = mongoose.model("User", userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { check, validationResult } = require('express-validator');
-const {signout, signup, signin, isSignedIn} = require("../controllers/auth");
+const { check, validationResult } = require("express-validator");
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
-
-
-router.post("/signup", [
-    check("name", "name should be atleast 3 characters").isLength({ min: 3 }),
+router.post(
+  "/signup",
+  [
+    check("name", "name should be at least 3 char").isLength({ min: 3 }),
     check("email", "email is required").isEmail(),
-    check("password", "password field is required").isLength({ min: 3 })
-] ,signup);
+    check("password", "password should be at least 3 char").isLength({ min: 3 })
+  ],
+  signup
+);
 
-router.post("/signin", [
+router.post(
+  "/signin",
+  [
     check("email", "email is required").isEmail(),
-    check("password", "password should be atleast 3 characters").isLength({ min: 1 })
-] ,signin);
-
-
+    check("password", "password field is required").isLength({ min: 1 })
+  ],
+  signin
+);
 
 router.get("/signout", signout);
+
+module.exports = router;
+
 
 /*router.get("/testroute", isSignedIn, (req, res) => {
     res.json(req.auth);
 
 })*/
-
-module.exports = router;
