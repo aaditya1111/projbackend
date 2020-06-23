8b93c72c1f8c08efae0db2b08f1f6a7c63952bee
Aaditya Kumar Rai <raiaaditya999@gmail.com>
2020-06-23 21:00:55 +0530

    commit

---
+++ controllers/category.js
@@ -0,0 +1,14 @@
+const Category = require("../models/category");
+
+exports.getCategoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, cate) =>{
+        if(err) {
+            return res.status(400).json({
+                error: "Category not found in DB"
+            });
+        }
+        req.Category = cate;
+        next();
+    });
+
+};
---
+++ routes/category.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {getCategoryById, createCategory} = require("../controllers/category");
+const {isSignedIn, isAdmin, isAuthenticated} = require("../controllers/auth");
+const {getUserById} = require("../controllers/user");
+
+//params
+router.param("userId", getUserById);
+router.param("categoryId", getCategoryById);
+
+
+//actual routes goes here
+router.post(
+    "/category/create/:userId", 
+    isSignedIn, 
+    isAuthenticated, 
+    isAdmin, 
+    createCategory
+);
+
+module.exports = router;    
