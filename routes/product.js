3a6b84c15a3091d2959c662678abc1c9421eb15e
Aaditya Kumar Rai <raiaaditya999@gmail.com>
2020-07-01 20:35:05 +0530

    commit

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, createProduct } = require("../controllers/product");
+const { getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct, getAllProducts } = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -10,6 +10,8 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 //all of actual routes
+
+//create route
 router.post(
   "/product/create/:userId",
   isSignedIn,
@@ -17,5 +19,20 @@ router.post(
   isAdmin,
   createProduct
 );
+
+//read route
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
+//delete route
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+
+
+//update route
+router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct);
+
+
+//listing route
+router.get("/products", getAllProducts);
 
 module.exports = router;
