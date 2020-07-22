6b5304386d37224da6f0da19593139fb75f2d31a
Aaditya Kumar Rai <raiaaditya999@gmail.com>
2020-07-22 13:55:49 +0530

    new commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ exports.signup = (req, res) => {
 
   if (!errors.isEmpty()) {
     return res.status(422).json({
-      error: errors.array()[0].msg
+      error: errors.array()[0].msg,
     });
   }
 
@@ -16,13 +16,13 @@ exports.signup = (req, res) => {
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: "NOT able to save user in DB"
+        err: "NOT able to save user in DB",
       });
     }
     res.json({
       name: user.name,
       email: user.email,
-      id: user._id
+      id: user._id,
     });
   });
 };
@@ -33,27 +33,27 @@ exports.signin = (req, res) => {
 
   if (!errors.isEmpty()) {
     return res.status(422).json({
-      error: errors.array()[0].msg
+      error: errors.array()[0].msg,
     });
   }
 
   User.findOne({ email }, (err, user) => {
     if (err || !user) {
       return res.status(400).json({
-        error: "USER email does not exists"
+        error: "USER email does not exists",
       });
     }
 
     if (!user.autheticate(password)) {
       return res.status(401).json({
-        error: "Email and password do not match"
+        error: "Email and password do not match",
       });
     }
 
     //create token
     const token = jwt.sign({ _id: user._id }, process.env.SECRET);
     //put token in cookie
-    res.cookie("token", token, { expire: new Date() + 9999 });
+    res.cookie("token", token, { expire: new Date() + 9999, httpOnly: false });
 
     //send response to front end
     const { _id, name, email, role } = user;
@@ -64,14 +64,14 @@ exports.signin = (req, res) => {
 exports.signout = (req, res) => {
   res.clearCookie("token");
   res.json({
-    message: "User signout successfully"
+    message: "User signout successfully",
   });
 };
 
 //protected routes
 exports.isSignedIn = expressJwt({
   secret: process.env.SECRET,
-  userProperty: "auth"
+  userProperty: "auth",
 });
 
 //custom middlewares
@@ -79,7 +79,7 @@ exports.isAuthenticated = (req, res, next) => {
   let checker = req.profile && req.auth && req.profile._id == req.auth._id;
   if (!checker) {
     return res.status(403).json({
-      error: "ACCESS DENIED"
+      error: "ACCESS DENIED",
     });
   }
   next();
@@ -88,7 +88,7 @@ exports.isAuthenticated = (req, res, next) => {
 exports.isAdmin = (req, res, next) => {
   if (req.profile.role === 0) {
     return res.status(403).json({
-      error: "You are not ADMIN, Access denied"
+      error: "You are not ADMIN, Access denied",
     });
   }
   next();
